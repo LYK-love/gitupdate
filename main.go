c6b30896ff2d3cfd18adcc56ea9e03dd35a3c29e
LYK-love <74082944+LYK-love@users.noreply.github.com>
2023-03-10 21:45:29 +0800

    defult?

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func removeDuplicates(elements []string) []string {
 func main() {
 	// Option to only consider top level folders
 	topLevelOnly := flag.Bool("top", false, "")
+	commitMessage := flag.String("m", "defult?", "set commit message, otherwise it will be auto generated")
 
 	args := os.Args
 	if len(args) < 2 {
@@ -39,14 +40,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	print(args[1]);
-	print(args[1:][0]);
-
+	//print(args[2])
+	print(args[1:][0])
 
 	if args[1:][0] == "." {
-		update(path, *topLevelOnly)
+		update(path, *topLevelOnly, *commitMessage)
 	} else {
-		update(path+"/"+args[1:][0], *topLevelOnly)
+		update(path+"/"+args[1:][0], *topLevelOnly, *commitMessage)
 	}
 
 }
@@ -54,7 +54,7 @@ func main() {
 // Track & update files in passed in path.
 // If it's folder, commit entire folder. If one file, commit the file.
 // Commit with file names as commit msg & push to remote.
-func update(path string, topLevelOnly bool) {
+func update(path string, topLevelOnly bool, commitMessage string) {
 	cmd := exec.Command("git")
 	file, err := os.Stat(path)
 	if err != nil {
@@ -95,10 +95,13 @@ func update(path string, topLevelOnly bool) {
 			filesChanged = removeDuplicates(filesChanged)
 
 			// Commit with a message
-			commitMsg := strings.Join(filesChanged, " ")
+			if commitMessage == "" {
+				commitMessage = strings.Join(filesChanged, " ")
+			}
+
 			cmd = exec.Command("git")
 			cmd.Dir = path
-			cmd.Args = []string{"git", "commit", "-m", commitMsg}
+			cmd.Args = []string{"git", "commit", "-m", commitMessage}
 			_, err = cmd.Output()
 			if err != nil {
 				log.Fatal(err)
